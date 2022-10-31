c70672f0cecdcaa9e5488940ba953fbc8cee1bb2
Mastermind2025 <85400723+Mastermind2025@users.noreply.github.com>
2022-11-01 00:45:36 +0200

    zzz

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+import lodash from 'lodash';
+import Player from '@vimeo/player';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const TIME = "videoplayer-current-time";
+
+
+player.on('timeupdate', lodash.throttle(onPlay, 1000));
+
+function onPlay({ seconds }) {
+    localStorage.setItem(TIME, seconds);
+    console.log('played the video!');
+    console.log('time is ->', seconds);
+};
+
+setCurrentTime();
+function setCurrentTime() {
+    if (!localStorage.getItem(TIME)) {
+        return;
+    }
+    player.setCurrentTime(localStorage.getItem(TIME));
+}
+
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,54 @@
+import lodash from 'lodash';
+import '../css/03-feedback.css';
 
+
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    textarea: document.querySelector('.feedback-form textarea'),
+    email: document.querySelector('.feedback-form input'),
+};
+const STORAGE_OBJ = 'feedback-form-state';
+
+const formData = {};
+
+refs.form.addEventListener('submit', onFormSubmit);
+
+refs.form.addEventListener('input', lodash.throttle(onFormInput, 500));
+
+
+favorite();
+
+function onFormSubmit(e) { 
+    e.preventDefault();
+
+    console.log('send form');
+    console.log(formData)
+    e.currentTarget.reset();
+    localStorage.removeItem(STORAGE_OBJ);
+    
+}
+
+//object
+function onFormInput(event) {
+    formData[event.target.name] = event.target.value;
+    localStorage.setItem(STORAGE_OBJ, JSON.stringify(formData));
+    
+}
+
+function favorite(e) {
+    const savedInfo = localStorage.getItem(STORAGE_OBJ);
+    const parsedInfo = JSON.parse(savedInfo);
+    if (parsedInfo) {
+        console.log('parsedInfo ->', parsedInfo);
+        if (parsedInfo.email) {
+            refs.email.value = parsedInfo.email;
+            
+        }
+        if (parsedInfo.message) {
+            refs.textarea.value = parsedInfo.message;
+        }
+    }
+    
+
+
+}
